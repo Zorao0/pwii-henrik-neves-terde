6843fbef7fa84578c4d2af0c676b7608e19f3a23
Zorao0 <zoraodamassa321@gmail.com>
2024-08-06 14:26:39 -0300

    Coloca os execicios das Olimpiadas no repositório

---
+++ aula01/7.js
@@ -0,0 +1,22 @@
+//Questão 1
+
+/*
+R = receita em dólares = -3p^2+180p
+p = preço
+*/
+
+pMaximo = 180/(2*-3)
+console.log("O lucro máximizado em dólares é:",pMaximo)
+console.log("O lucro máximizado em reais é:",pMaximo*5,65)
+
+// Questão 2
+
+//Sendo p = -30
+p = -30
+valorD = (-3*p^2+180*p)
+valorR = valorD*5,65
+
+console.log("O lucro máximizado em dólares é:",valorD)
+console.log("O lucro máximizado em reais é:",valorR)
+
+//Os resultados foram diferentes do dos resultados do pdf, pois, alguns valores das contas do arquivo estão erradas.
---
+++ aula01/8.js
@@ -0,0 +1,18 @@
+//Questão A
+
+t = 5
+distanciaC = 10*t
+distanciaD = -0.5*t^2+12*t
+
+if (distanciaC>distanciaD){
+    console.log("O corredor D percorre:",distanciaC,"em 5 segundos")
+}else{
+    console.log("O corredor D percorre:",distanciaD,"em 5 segundos")
+}
+
+//Questão B
+
+Dmaximo = 12/(2*-0.5)
+Distancia = -0.5*Dmaximo^2+12*Dmaximo
+
+console.log("A distância máxima é:",Distancia)
