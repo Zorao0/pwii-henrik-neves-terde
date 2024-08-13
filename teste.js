531075169fd75198787b7697fe05182798c9d1b6
Zorao0 <zoraodamassa321@gmail.com>
2024-08-13 14:37:24 -0300

    Corrige os exercícios

---
+++ teste.js
@@ -0,0 +1,10 @@
+let variavel1 = "oi"
+var variavel2 = "pessoal"
+const variavel3 = 3
+
+console.log(typeof variavel1)
+console.log(typeof variavel2)
+console.log(typeof variavel3)
+
+variavel1 = 1
+console.log()
